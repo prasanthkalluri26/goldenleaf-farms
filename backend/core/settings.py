4c00017da9f8a68bae3e1b2ea7c75084b6ab265d
prasanthkalluri26 <prasanthkalluri26@gmail.com>
2026-06-05 00:28:50 +0530

    Add Vercel frontend to CORS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -132,7 +132,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://goldenleaf-farms.vercel.app",
 ]
-
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
